665d034b16a44430055bf5521bba490d2c255715
david95j2 <glory20220j@gmail.com>
2021-04-10 00:46:09 +0900

    폰트 적용

--- src/App.vue
+++ src/App.vue
@@ -35,15 +35,3 @@ export default defineComponent({
   name: 'App',
 })
 </script>
-
-<style>
-html,body {
-  margin: 0;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>
